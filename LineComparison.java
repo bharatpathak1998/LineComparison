2dc3e98ac6325d898e5444c44c844d38f4279c3e
bharatpathak1998 <bharatpathak099@gmail.com>
2022-07-07 21:42:06 +0530

    initial commit

---
+++ LineComparison.java
@@ -0,0 +1,16 @@
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation");
+		 
+	    int x1=3,x2=5,y1=2,y2=6;
+	     
+	    double length = (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+	     
+	    System.out.printf("length of line is = %.2f ",length);
+
+	}
+
+}

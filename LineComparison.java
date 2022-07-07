cf094c99abc43b553daaf6f0c5b875d460393bc8
Bharat Pathak <107098833+bharatpathak1998@users.noreply.github.com>
2022-07-07 22:26:13 +0530

    Update LineComparison.java

--- LineComparison.java
+++ LineComparison.java
@@ -3,8 +3,8 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		
-		int x1=3,x2=5,y1=2,y2=6;
-		Double LengthOfFirstLine = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+	int x1=3,x2=5,y1=2,y2=6;
+        Double LengthOfFirstLine = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         
         int x3=3,x4=5,y3=2,y4=6;
         Double LengthOfSecondLine = Math.sqrt((y4 - y3) * (y4 - y3) + (x4 - x3) * (x4 - x3));
@@ -23,5 +23,4 @@ public class LineComparison {
         }   
 
 	}
-
 }

d5e4c3a6fc2ce1d6da6e08595af0e309be7d0381
Bharat Pathak <107098833+bharatpathak1998@users.noreply.github.com>
2022-07-22 01:31:52 +0530

    Update LineComparison.java

--- LineComparison.java
+++ LineComparison.java
@@ -1,14 +1,14 @@
 
 public class LineComparison {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-            System.out.println("Welcome to Line Comparison Computation");
+        System.out.println("Welcome to Line Comparison Computation");
 		 
-	    int x1=3,x2=5,y1=2,y2=6;
+	int x1=3,x2=5,y1=2,y2=6;
 	     
-	    double length = (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+	double length = (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
 	     
-	    System.out.printf("length of line is = %.2f ",length);
-	}
+	System.out.printf("length of line is = %.2f ",length);
+    }
 }
